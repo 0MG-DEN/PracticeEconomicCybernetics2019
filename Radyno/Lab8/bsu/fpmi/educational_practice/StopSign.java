d31901b05218141982cc0d5bcf4ecd85e3c8ef5a
unknown <loooool2312@gmail.com>
2019-04-30 19:30:33 +0300

    Lab8

---
+++ Radyno/Lab8/bsu/fpmi/educational_practice/StopSign.java
@@ -0,0 +1,46 @@
+package bsu.fpmi.educational_practice;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class StopSign extends Canvas {    
+    private int diameter = 120;
+    private Color backgroundColor = Color.DARK_GRAY;
+    private Color signColor = Color.LIGHT_GRAY;
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public Color getSignColor() {
+        return signColor;
+    }
+
+    public void setSignColor(Color signColor) {
+        this.signColor = signColor;
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(backgroundColor);
+        g.fillOval(0, 0, diameter, diameter);
+        final int rectX = (diameter >> 1) - (diameter >> 2);
+        final int rectY = (diameter >> 1) - (diameter >> 3);
+        g.setColor(signColor);
+        g.fillRect(rectX, rectY, diameter >> 1, diameter >> 2);
+    }
+}
